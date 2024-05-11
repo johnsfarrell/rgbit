6e945d798e348f0c0181ef676ab9d8b1c4a9a02d
John Farrell <jsfarrell12@gmail.com>
2024-05-11 12:10:01 -0400

    feat: dynamic img size

--- ml/code/visualize.py
+++ ml/code/visualize.py
@@ -1,10 +1,7 @@
 from skimage.io import imread
 import matplotlib.pyplot as plt
-from skimage.color import rgb2lab, lab2rgb
-import numpy as np
 from skimage.io import imread
-from util import load_model
-from skimage.transform import resize
+from util import load_model, resize_image, fix_dims, predict_color, upscale_color
 import hyperparameters as hp
 
 MODEL = load_model()
@@ -16,34 +13,25 @@ def visualize_image(image_path):
     Visualizes the original image (ground truth), grayscale image, and the predicted image.
     """
     img = imread(image_path)
-    img = resize(img, output_shape=(hp.img_size, hp.img_size, 3))
-    img / 255.0
-    image_lab = rgb2lab(img)
-    image_l = image_lab[:, :, [0, 0, 0]]
+    img = fix_dims(img)
 
-    # Run the model on image_l to get predicted ab channels
-    image_ab = MODEL.predict(image_l[np.newaxis, ...])
-    print(image_ab)
-    image_ab = image_ab[0]
-    image_lab[:, :, [1, 2]] = image_ab
-    image_rgb_predicted = lab2rgb(image_lab * 1.3)
-    print(image_rgb_predicted)
+    downscaled_img = resize_image(img, (hp.img_size, hp.img_size))
+    colored_lab = predict_color(downscaled_img, MODEL)
+    upscaled_rgb = upscale_color(img, colored_lab)
+
     # Create a single row plot
     fig, axs = plt.subplots(1, 3, figsize=(15, 5))
     # Plot each image
-    axs[0].imshow(image_l / 100.0)
+    axs[0].imshow(colored_lab[:, :, 0], cmap='gray')
     axs[0].set_title('Image L')
 
     axs[1].imshow(img)
     axs[1].set_title('Original Image')
 
-    axs[2].imshow(image_rgb_predicted)
+    axs[2].imshow(upscaled_rgb)
     axs[2].set_title('Predicted RGB Image')
 
     plt.tight_layout()
-    plt.show()
-
-    plt.imshow(image_ab[:, :, 0], cmap="gray")
     plt.show()
 
 """
