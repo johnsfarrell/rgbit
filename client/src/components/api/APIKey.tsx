a8c86a2a4716abb00456844c1d77f76ea159230a
John Farrell <jsfarrell12@gmail.com>
2024-05-16 19:53:07 -0400

    fix: APIKey not showing new key

--- client/src/components/api/APIKey.tsx
+++ client/src/components/api/APIKey.tsx
@@ -9,9 +9,11 @@ import {
 import { getKey } from "../../utils/cookie";
 import { COPY_TOAST } from "../../utils/toasts";
 import { CopyIcon } from "@chakra-ui/icons";
+import { useEffect, useState } from "react";
+import { generateAPIKey } from "../../utils/key";
 
 const APIKey = () => {
-  const key = getKey();
+  const [key, setKey] = useState<string | undefined>("...");
   const toast = useToast();
 
   const handleCopy = () => {
@@ -20,10 +22,16 @@ const APIKey = () => {
     toast(COPY_TOAST);
   };
 
+  useEffect(() => {
+    const apiKey = getKey();
+    if (apiKey) setKey(apiKey);
+    else generateAPIKey().then(({ key }) => setKey(key));
+  }, []);
+
   return (
     <InputGroup fontFamily="monospace" maxW={500}>
       <InputLeftAddon>key=</InputLeftAddon>
-      <Input type="text" value={key || "..."} isTruncated={true} readOnly />
+      <Input type="text" value={key} isTruncated={true} readOnly />
       <InputRightElement>
         <Button onClick={handleCopy} border="1px solid #e2e8f0" roundedLeft={0}>
           <CopyIcon />
