8f16461091d5702ce7dfe34e52b87eab94a24265
John Farrell <jsfarrell12@gmail.com>
2024-05-18 01:04:35 -0400

    fix: menu click

--- client/src/components/menu/MenuTitle.tsx
+++ client/src/components/menu/MenuTitle.tsx
@@ -3,22 +3,24 @@ import { ACTIVE_HOVER, HUE_ROTATE } from "../../utils/animations";
 
 const MenuTitle = () => {
   return (
-    <Box
-      as="h1"
-      position="absolute"
-      display={{ base: "none", sm: "block" }}
-      bgGradient="linear(125deg, #a7271e, #a18f1f, #519519)"
-      bgClip="text"
-      color="transparent"
-      animation={HUE_ROTATE}
-      fontWeight="900"
-      p="10px 20px"
-      transition="all 0.2s"
-      _hover={{ cursor: "pointer", ...ACTIVE_HOVER }}
-      textTransform="lowercase"
-    >
-      <a href={process.env.PUBLIC_URL || "#"}>RGBit!</a>
-    </Box>
+    <a href={process.env.PUBLIC_URL}>
+      <Box
+        as="h1"
+        position="absolute"
+        display={{ base: "none", sm: "block" }}
+        bgGradient="linear(125deg, #a7271e, #a18f1f, #519519)"
+        bgClip="text"
+        color="transparent"
+        animation={HUE_ROTATE}
+        fontWeight="900"
+        p="10px 20px"
+        transition="all 0.2s"
+        _hover={{ cursor: "pointer", ...ACTIVE_HOVER }}
+        textTransform="lowercase"
+      >
+        RGBit!
+      </Box>
+    </a>
   );
 };
 
